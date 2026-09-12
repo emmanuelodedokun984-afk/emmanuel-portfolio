707008cfc3a81c48314c95a67f58416cb6ee6bd5
emmanuelodedokun984-afk <emmanuelodedokun984@gmail.com>
2026-09-12 23:05:01 +0100

    Simplify backToTop scroll behavior

--- script.js
+++ script.js
@@ -64,8 +64,6 @@ window.addEventListener("scroll", () => {
 });
 
 backToTop.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+    window.scrollTo(0, 0);
     });
 });
